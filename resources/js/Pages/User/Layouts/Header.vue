fa9bcdb236f959cec3e852d9485f6cd81c3a9352
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2025-01-14 20:16:17 -0500

    miniFix

--- resources/js/Pages/User/Layouts/Header.vue
+++ resources/js/Pages/User/Layouts/Header.vue
@@ -95,7 +95,7 @@ const cart = computed(() => usePage().props.cart);
                         :class="[
                             'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700',
                             {
-                                'md:text-blue-700': route().current('evaluations.index') || route().current('evaluations.quiz') ,
+                                'md:text-blue-700': route().current('evaluations.index') || route().current('evaluations.quiz') || route().current('evaluations.index.admin'),
                             },
                         ]"    
                         aria-current="page">Evaluaciones</Link>
